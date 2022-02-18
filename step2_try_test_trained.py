e81aab6d0523ce741df544e0646ee8541290131e
guang384 <guang384@qq.com>
2022-02-18 17:02:42 +0800

    Optimize the logic of file names

---
+++ step2_try_test_trained.py
@@ -0,0 +1,52 @@
+# 尝试把训练好的模型用作预测查看效果
+
+import sys
+
+import numpy as np
+import torch
+from elegantrl import evaluator, config
+from elegantrl.agent import AgentModSAC
+from elegantrl.config import get_gym_env_args, Arguments
+
+import gym
+
+gym.logger.set_level(40)  # Block warning
+
+
+def test_model(actor_path="LunarLanderContinuous-v2_ModSAC_0/actor_00200542_00221.985.pth"):
+    get_gym_env_args(gym.make("LunarLanderContinuous-v2"), if_print=True)
+
+    env_func = gym.make
+    env_args = {
+        "env_num": 1,
+        "env_name": "LunarLanderContinuous-v2",
+        "max_step": 1000,
+        "state_dim": 8,
+        "action_dim": 2,
+        "if_discrete": False,
+        "target_return": 200,
+        "id": "LunarLanderContinuous-v2",
+    }
+
+    # 初始化
+    agent = AgentModSAC
+    args = Arguments(agent, env_func=env_func, env_args=env_args)
+    gpu_id = args.learner_gpus
+    env = config.build_env(env_func=env_func, env_args=env_args)
+    # 加载训练好的模型
+    act = agent(args.net_dim, env.state_dim, env.action_dim, gpu_id=gpu_id, args=args).act
+    act.load_state_dict(torch.load(actor_path, map_location=lambda storage, loc: storage))
+    torch.no_grad()
+    # 测试
+    r_s_ary = [evaluator.get_episode_return_and_step(env, act) for _ in range(args.eval_times)]
+    r_s_ary = np.array(r_s_ary, dtype=np.float32)
+    r_avg, s_avg = r_s_ary.mean(axis=0)  # average of episode return and episode step
+    print("run %d rounds. r_avg = %f  s_avg = %f" % (args.eval_times, r_avg, s_avg))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        argv_actor_path = sys.argv[1]
+        test_model(argv_actor_path)
+    else:
+        test_model()
---
+++ step9_test_trained.py
@@ -0,0 +1,56 @@
+# 尝试训练
+import sys
+
+from elegantrl.agent import AgentDQN, AgentDoubleDQN, AgentD3QN, AgentDiscretePPO
+from elegantrl.config import get_gym_env_args, Arguments
+from elegantrl.run import train_and_evaluate, train_and_evaluate_mp
+
+import gym
+from gym import register
+
+from tiny_market import profits_or_loss_reward, linear_fine
+
+register(
+    id='TinyMarketGymEnvRandom-v0',
+    entry_point='tiny_market:GymEnvRandom',
+    max_episode_steps=3600,  # 一个episode最大步数
+    reward_threshold=1000000.0,
+)
+
+gym.logger.set_level(40)  # Block warning
+
+
+def try_train(file_path="data/dominant_processed_data_20170103_20220215.h5"):
+    env_args = get_gym_env_args(gym.make("TinyMarketGymEnvRandom-v0"), if_print=False)
+
+    def make_env_func(**kwargs):
+        env = gym.make(env_args['env_name'])
+        env.init(capital=20000,
+                 file_path=file_path,
+                 date_start="20211201", date_end="20211231",
+                 reward_func=profits_or_loss_reward,
+                 fine_func=linear_fine(0.1)
+                 )
+        return env
+
+    args = Arguments(AgentDQN, env_func=make_env_func, env_args=env_args)
+
+    # DQN, DoubleDQN, D3QN, PPO-Discrete for discrete actions
+    # AgentDQN
+    # AgentDoubleDQN
+    # AgentD3QN
+    # AgentDiscretePPO
+
+    args.target_step = args.max_step
+    args.gamma = 0.99
+    args.eval_times = 10
+    args.if_remove = False
+    train_and_evaluate(args)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        argv_file_path = sys.argv[1]
+        try_train(argv_file_path)
+    else:
+        try_train()
