b2a4541b36d0ab067a8171292a7b82f0e209c96c
guang384 <guang384@qq.com>
2022-03-02 21:19:30 +0800

    update to multiprocessing and gpu version

--- early-immature-ideas/study-demo/embeding/word2vec.py
+++ early-immature-ideas/study-demo/embeding/word2vec.py
@@ -1,4 +1,5 @@
 # PyTorch 实现 Word2Vec https://wmathor.com/index.php/archives/1435/
+import timeit
 
 import torch
 import torch.nn as nn
@@ -11,18 +12,21 @@ import random
 import scipy
 from sklearn.metrics.pairwise import cosine_similarity
 
+C = 3  # context window
+K = 15  # number of negative samples pre positive
+EPOCHS = 1
+MAX_VOCAB_SIZE = 10000
+EMBEDDING_SIZE = 100
+BATCH_SIZE = 32
+DATALADER_WORKER_NUMS = 3  # 数据集采样程序GPU加成不足，不如增多CPU进程数效率高
+LR = 1e-3
 SEED = 10086
+
 random.seed(SEED)
 np.random.seed(SEED)
 torch.manual_seed(SEED)
 
-C = 3  # context window
-K = 15  # number of negative samples pre positive
-epochs = 2
-MAX_VOCAB_SIZE = 10000
-EMBEDDING_SIZE = 100
-batch_size = 32
-lr = 0.2
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # 读取文本
 with open('data/text8.train.txt') as f:
@@ -53,11 +57,11 @@ class WordEmbeddingDataset(tud.Dataset):
                  text,
                  word2idx,
                  word_freqs):
-        '''
+        """
         text: 拆分成单词的语料
         word2idx: 单词到索引号（编号）的映射
         word_freqs: 词频
-        '''
+        """
         super(WordEmbeddingDataset, self).__init__()
 
         # 编码后的语料
@@ -73,12 +77,12 @@ class WordEmbeddingDataset(tud.Dataset):
         return len(self.text_encoded)
 
     def __getitem__(self, idx):
-        '''
+        """
         返回：
         - 中心词
         - 相关词 positive word
         - 随机采样得到的 negative word  ( K倍的positive word )
-        '''
+        """
         center_word = self.text_encoded[idx]
 
         positive_indices = list(range(idx - C, idx)) + list(range(idx + 1, idx + C + 1))
@@ -86,22 +90,16 @@ class WordEmbeddingDataset(tud.Dataset):
 
         positive_words = self.text_encoded[positive_indices]  # positive_indices是个列表，返回的会是个tensor
 
-        negative_words = torch.multinomial(self.word_freqs, K * positive_words.shape[0], True)  # 多项式分布抽样 K倍正向词数量
+        # 多项式分布抽样 K倍正向词数量
+        negative_words = torch.multinomial(self.word_freqs, K * positive_words.shape[0], True)
 
-        # wihle循环确保正负向词无交集
+        # While 循环确保正负向词无交集(GPU下用tensor算交集
         while len(
-                set(positive_words.numpy().tolist()) & set(negative_words.numpy().tolist())  # 取交集
+                set(negative_words.cpu().numpy().tolist()) & set(positive_words.cpu().numpy().tolist())  # 取交集
         ) > 0:
             negative_words = torch.multinomial(self.word_freqs, K * positive_words.shape[0], True)
 
         return center_word, positive_words, negative_words
-
-
-dataset = WordEmbeddingDataset(text, word2idx, word_freqs)
-dataloader = tud.DataLoader(dataset, batch_size, shuffle=True)
-
-
-# print(next(iter(dataset)))  # 验证数据集可用
 
 
 # 定义模型
@@ -115,14 +113,16 @@ class EmbeddingModel(nn.Module):
         self.input_embed = nn.Embedding(self.vocab_size, self.embed_size)
         self.output_embed = nn.Embedding(self.vocab_size, self.embed_size)
 
+        self.to(device)
+
     def forward(self, input_labels, positive_labels, negative_labels):
-        '''
+        """
         input_labels: center words,    shape:  [batch_size]
         positive_labels: positive words,  shape: [batch_size, context window size * 2]   乘以二是因为有前文后文两部分
         negative_labels: negative words, shape: [batch_size, K * context window size * 2]
 
         return loss, shape:[batch_size]
-        '''
+        """
         input_embedding = self.input_embed(input_labels)  # shape: [batch_size,embed size]
         positive_embedding = self.output_embed(positive_labels)  # [batch_size, (window * 2), embed_size]
         negative_embedding = self.output_embed(negative_labels)  # [batch_size, (window * 2 * K), embed_size]
@@ -147,37 +147,59 @@ class EmbeddingModel(nn.Module):
         return self.input_embed.weight.detach().numpy()
 
 
-model = EmbeddingModel(MAX_VOCAB_SIZE, EMBEDDING_SIZE)
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
-
 # 训练
+def train():
+    dataset = WordEmbeddingDataset(text, word2idx, word_freqs)
+    dataloader = tud.DataLoader(dataset, BATCH_SIZE, shuffle=True, num_workers=DATALADER_WORKER_NUMS)
+    print('dataset word size: ', len(dataset))
 
-for e in range(epochs):
-    for i, (input_labels, positivate_labels, negative_lables) in enumerate(dataloader):
-        input_labels = input_labels.long()
-        positivate_labels = positivate_labels.long()
-        negative_lables = negative_lables.long()
+    model = EmbeddingModel(MAX_VOCAB_SIZE, EMBEDDING_SIZE)
+    print('model on cuda : ', next(model.parameters()).is_cuda)
+    optimizer = torch.optim.Adam(model.parameters(), lr=LR)
+    for e in range(EPOCHS):
+        last_time = timeit.default_timer()
+        calc_time = 0
+        step = 0
+        for i, (input_labels, positive_labels, negative_labels) in enumerate(dataloader):
+            start = timeit.default_timer()
+            input_labels = input_labels.long().to(device)
+            positive_labels = positive_labels.long().to(device)
+            negative_labels = negative_labels.long().to(device)
 
-        optimizer.zero_grad()
-        loss = model(input_labels, positivate_labels, negative_lables).mean()
-        loss.backward()
-        optimizer.step()
+            optimizer.zero_grad()
+            loss = model(input_labels, positive_labels, negative_labels).mean()
+            loss.backward()
+            optimizer.step()
+            use = timeit.default_timer() - start
+            calc_time += use
+            if i % 100 == 0:
+                time = timeit.default_timer() - last_time
+                print('EPOCH: ', e, '| ITERATION: ', i, '| LOSS: ', loss.item(), '| DELTA TIME: ', time,
+                      '| CALC Time: ',
+                      calc_time)
+                calc_time = 0
+                last_time = timeit.default_timer()
 
-        if i % 100 == 0:
-            print('epoch', e, 'iteration', i, loss.item())
-
-embedding_weights = model.input_embedding()
-torch.save(model.state_dict(), "")
-torch.save(model.state_dict(), "embedding-{}.th".format(EMBEDDING_SIZE))
+    embedding_weights = model.input_embedding()
+    torch.save(model.state_dict(), "")
+    torch.save(model.state_dict(), "embedding-{}.th".format(EMBEDDING_SIZE))
+    return embedding_weights
 
 
 # 验证
-def find_nearest(word):
-    index = word2idx[word]
-    embedding = embedding_weights[index]
-    cos_dis = np.array([scipy.spatial.distance.cosine(e, embedding) for e in embedding_weights])
-    return [idx2word[i] for i in cos_dis.argsort()[:10]]
+def test(embedding_weights):
+    def find_nearest(word):
+        index = word2idx[word]
+        embedding = embedding_weights[index]
+        cos_dis = np.array([scipy.spatial.distance.cosine(e, embedding) for e in embedding_weights])
+        return [idx2word[i] for i in cos_dis.argsort()[:10]]
+
+    for word in ["two", "america", "computer"]:
+        print(word, find_nearest(word))
 
 
-for word in ["two", "america", "computer"]:
-    print(word, find_nearest(word))
+if __name__ == '__main__':
+    print('run on device : ', device)
+
+    embedding_weights = train()
+    test(embedding_weights)
